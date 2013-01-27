b868ddf863bafffeb2d24fb34f6b1ec37549805e
Jacqui Hawkins <jk.hawkins.jk@gmail.com>
2013-01-27 11:49:54 +1100

    Fix for negative health. Hunter template.

---
+++ Hunter.cpp
@@ -0,0 +1,17 @@
+// (C) 2013 Tim Gurto
+
+#include <cassert>
+#include "Hunter.h"
+#include "Point.h"
+#include "Debug.h"
+#include "util.h"
+
+namespace Game {
+
+extern Debug debug;
+
+    Hunter::Hunter() : Person(Point(0,0))
+	{
+	}
+
+} //namespace Game
---
+++ Hunter.h
@@ -0,0 +1,17 @@
+// (C) 2013 Tim Gurto
+
+#ifndef HUNTER_H
+#define HUNTER_H
+
+#include "Person.h"
+
+namespace Game {
+
+	class Hunter : public Person {
+	public:
+		Hunter();
+	};
+
+} //namespace Game
+
+#endif
